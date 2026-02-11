a5c61a59e13df89157bbbaa1bc6c5544b4ef38cf
Lucia <luciamoneoo@gmail.com>
2026-02-11 21:20:12 +0100

    RA6 terminado

--- src/main/java/pio/daw/App.java
+++ src/main/java/pio/daw/App.java
@@ -11,8 +11,28 @@ public class App {
      * @return Path to file if exists.
      */
     public static Path getPathFromArgs(String[] args){
-        //TODO
-        return null;
+
+        //primero deberíamos ver si el path existe
+        //entonces, con un if, veremos si hay argumento o no, sino, saldremos del programa
+        // la lonitud del argumeno será de 1 que será el nombre del archivo que tieen los registros
+        if(args.length != 1){
+            System.out.println(" Pase la ruta del archivo: ");
+            System.exit(1);
+        } 
+        // ahora crearemos un objeto path con el argumento que le damos al programa
+        // luego veremos si el archivo existe o no, si no existe saldremos
+        //(una vez ya hemos comprobado arriba que el argumento es correcto)
+
+        Path path = Path.of(args[0]);
+
+        //en el if, convertimos el path en file, ya que el exists() solo funciona con filess, y 
+        // ese metodo funciona como un boolean, devoviendo true si el file existe, o false si no
+        if(!path.toFile().exists()){
+            System.out.println(" El archivo no existe: " + path);
+            System.exit(1);
+        }
+       
+        return path;
     }
 
     public static void main(String[] args) {
